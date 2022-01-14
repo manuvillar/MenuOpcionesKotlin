301e334b3196ccacd71f3a0ffc544579fc901fe2
Manu Villar <mjvillar@iesoretania.es>
2022-01-14 12:46:10 +0100

    Finalización de la funcionalidad de la app.

--- app/src/main/java/es/iesoretania/menuopcioneskotlin/MainActivity.kt
+++ app/src/main/java/es/iesoretania/menuopcioneskotlin/MainActivity.kt
@@ -3,8 +3,11 @@ package es.iesoretania.menuopcioneskotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,22 +15,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Registramos el menú contextual
         registerForContextMenu(idimageView1)
     }
 
+    //Creamos ese menú contextual.
     override fun onCreateContextMenu(
         menu: ContextMenu?,
         v: View?,
         menuInfo: ContextMenu.ContextMenuInfo?
     ) {
         super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
     }
 
+    //Controlamos las acciones de ese menú.
     override fun onContextItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.idimageView1 -> return true
-            R.id.idimageView2 -> return true
-            else -> return super.onContextItemSelected(item)
+        return when (item.itemId) {
+            R.id.idmenu1 -> {
+                Toast.makeText(this, "Opción 1", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.idmenu2 -> {
+                Toast.makeText(this, "Opción 2", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onContextItemSelected(item)
         }
     }
 }
